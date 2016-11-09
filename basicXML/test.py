1594c9ee3b28e556dab38c69f6860f8ec107b8a6
Joshua Shapiro <jshapiro314@gwu.edu>
2016-11-09 15:52:17 -0500

    Added cylinder and moved table in scene for demo of grasp, still can't show grasp action

--- basicXML/test.py
+++ basicXML/test.py
@@ -7,13 +7,33 @@ env.SetViewer('qtcoin') # attach viewer (optional)
 env.Load('./testEnv.xml') # load a simple scene
 time.sleep(10)
 robot = env.GetRobots()[0] # get the first robot
-manipulator = robot.GetManipulators()[0]
+#manipulator = robot.GetManipulators()[0]
 
-robot.SetActiveManipulator(manipulator)
+#robot.SetActiveManipulator(manipulator)
 target = env.GetKinBody("block")
 gmodel = databases.grasping.GraspingModel(robot,target)
-if not gmodel.load():
-	gmodel.autogenerate()
+gmodel.autogenerate()
+
+validgrasps, validindicees = gmodel.computeValidGrasps(startindex=0, checkcollision=True, checkik=True, checkgrasper=True, backupdist=0.0, returnnum=1)
+
+print(validgrasps)
+
+target = env.GetKinBody("cylinder")
+gmodel = databases.grasping.GraspingModel(robot,target)
+gmodel.autogenerate()
+validgrasps, validindicees = gmodel.computeValidGrasps(startindex=0, checkcollision=True, checkik=True, backupdist=0.0, returnnum=1)
+
+print(validgrasps)
+
+robot.Grab(target)
+gmodel.moveToPreshape(validgrasps[0])
+Tgoal = gmodel.getGlobalGraspTransform(validgrasps[0],collisionfree=True)
+basemanip = interfaces.BaseManipulation(robot)
+basemanip.MoveToHandPosition(matrices=[Tgoal])
+robot.WaitForController(0)
+taskmanip = interfaces.TaskManipulation(robot)
+taskmanip.CloseFingers()
+robot.WaitForController(0)
 
 with env: # lock the environment since robot will be used
 	raveLogInfo("Robot "+robot.GetName()+" has "+repr(robot.GetDOF())+" joints with values:\n"+repr(robot.GetDOFValues()))
